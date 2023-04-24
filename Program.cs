a9d0b904e8465db361baedce72a366ed9b339d0c
Your Name <you@example.com>
2023-04-24 10:18:18 +0700

    lp

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using system
+Class program {
+    static void Main (String[] args) {
+        Console.Writeline("Input item massage")
+        Console.Read("Item massage") {
+            int = item massage
+            Console.Writeline("Name item")
+            Console.Read("Name item") {
+                int = item massage
+                Console.Writeline("Type item")
+                Console.Read("Type item")
+            }
+        }
+}
